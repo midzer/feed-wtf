95fad55eb3390eddbcf7b5125007831d483086e5
midzer <midzer@gmail.com>
2021-05-27 18:00:51 +0200

    add hint

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,3 @@
 <p>feed.wtf ist ein Gemeinschaftsprojekt, um RSS-/JSON-Feeds allen zur Verfügung zu stellen.</p>
 <p>Ihr könnt eigene und stadtbezogene Feeds abrufen und eintragen.</p>
+<p><b>Hinweis</b>: Bisher gibt es nur 3 Ort nach denen ihr suchen könnt: "Augsburg", "Dachau" und "München"</p>
