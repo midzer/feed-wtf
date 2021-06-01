cb2664b2321b9b68731d1c2e31f220b76f657efe
midzer <midzer@gmail.com>
2021-06-01 17:15:37 +0200

    improve main text

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,3 @@
-<p>feed.wtf is a community-driven project to make RSS/JSON feeds available to everyone. You can retrieve and enter your own and city feeds.</p>
+<p><b>feed.wtf</b> is a community-driven project to make RSS/JSON feeds available to everyone.</p>
+<p>For individual user pages you are free to customize your feed. Whereas city pages should contain only local authorities, associations, blogs and newspapers feeds.</p>
+<p><b>Notice</b>: Right now, it is not possible to remove entered feeds directly. Leave me a message at mail[at]midzer[dot]de and I can help you out.</p>
